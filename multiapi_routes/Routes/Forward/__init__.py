a0b22c56ab7940b5be104dfc7c9ad3e998ea7900
vortex <vortex@debian.lan>
2023-11-16 12:26:09 +0000

    WebSocket Exemple Fixed!

--- multiapi_routes/Routes/Forward/__init__.py
+++ multiapi_routes/Routes/Forward/__init__.py
@@ -106,7 +106,8 @@ class forward(APIRouter):
                     message.appendChild(content)
                     messages.appendChild(message)
                 };
-                
+                event.preventDefault()
+            }
             function sendMessage(event) {
                 var input = document.getElementById("messageText")
                 ws.send(input.value)
