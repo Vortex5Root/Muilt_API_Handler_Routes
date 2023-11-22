4ebde50683a5d2aae4474113a91601ebcbea70fd
vortex <vortex@debian.lan>
2023-11-22 13:42:14 +0000

    Ws Exemple location.host

--- multiapi_routes/Routes/Forward/__init__.py
+++ multiapi_routes/Routes/Forward/__init__.py
@@ -109,7 +109,7 @@ class forward(APIRouter):
                 function connect(event) {
                     var modelId = document.getElementById("modelId").value;
                     var token = document.getElementById("token").value;
-                    ws = new WebSocket("ws://"+location.hostnamet+":8000/v1/forward/"+modelId+"/stream?token="+token);
+                    ws = new WebSocket("ws://"+window.location.host+":8000/v1/forward/"+modelId+"/stream?token="+token);
                     ws.onmessage = function(event) {
                         var messages = document.getElementById('messages');
                         var message = document.createElement('li');
